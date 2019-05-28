6c043e6e311fd6424baac6071c1ccef6ee0ec15f
和也山本 <kazuya2137@gmail.com>
2019-05-28 23:38:01 +0800

    課題

--- php_practice.php
+++ php_practice.php
@@ -7,3 +7,72 @@ $var = 0;
     for ($i = 100; $i >1; $i--) {
     echo $i . '<br>';
     }
+// question2
+    for ($i = 1; $i <9 ; $i++){
+        echo $i * 3 . '<br>';
+    }
+    // question3
+    for ($i = 1; $i <= 100; ++$i){
+        if ($i % 3 === 0 && $i % 5 ===0){
+            echo 'FizzBuzz';
+        } elseif ($i % 5 === 0) {
+            # code...
+            echo 'Buzz';
+        } elseif ($i % 3 === 0) {
+            echo'Fizz';
+            # code...
+        } else{
+            echo $i;
+        }
+        echo'<br>';
+    }
+    // question4
+    for ($i=1; $i <= 100 ; $i++) { 
+        echo $i.' ';
+        if($i % 10 === 0){
+            echo'<br>';
+        }
+    }
+    // question5
+    $alpha = ['E', 'A', 'D', 'B', 'A', 'C', 'A', 'B', 'E', 'E', 'A', 'A', 'C'];
+    $i = 0;
+
+    foreach ($alpha as $v) {
+        if ($v === 'A'){
+            $i++;
+        }
+        
+    }
+    // questin6
+    $english = 70;
+    $math = 70;
+    $total = $english + $math;
+
+    if ($english >= 60 && $math >= 60 && $total >=140) {
+        echo '合格';
+
+    }else{
+        echo'残念';
+    }
+    // questin7
+    for ($i = 1; $i <= 9; $i++){
+        for($j = 1; $j <= 9; $j++){
+            $res = $i * $j . ' ';
+            echo $res;
+        }
+        echo "<br>";
+    }
+    // question8
+    $primeNumbers = [];
+    for ($i = 2; $i < 100; $i++) {
+        for($j = 2; $j<= $i; $j++) {
+            if ($i % $j === 0 && $j < $i){
+            break;
+        }
+        if ($i === $j){
+            $primeNumbers[] = $i;
+            }
+        }
+    }
+    print_r($primeNumbers);
+
